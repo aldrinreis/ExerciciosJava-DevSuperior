45c6915ca0c8c964870322d46e5a28a7990c7f92
ALDRIN REIS DE MORAIS <40066614+aldrinreis@users.noreply.github.com>
2024-12-25 17:06:57 -0300

    Inse

--- src/oneEstruturaSequencial/aProblemaTerreno.java
+++ src/oneEstruturaSequencial/aProblemaTerreno.java
@@ -1,12 +1,15 @@
-package oneEstruturaSequencial;/*
+/*
 * Fazer um programa para ler as medidas da largura e comprimento de um terreno retangular com uma casa decimal, bem como o valor do metro quadrado do terreno com duas casas decimais.
 * Em seguida, o programa deve mostrar o valor da área do terreno, bem como o valor do preço do terreno, ambos com duas casas decimais
 */
 
+package oneEstruturaSequencial;
+
+
 import java.util.Locale;
 import java.util.Scanner;
 
-public class oneTerreno {
+public class aProblemaTerreno {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
