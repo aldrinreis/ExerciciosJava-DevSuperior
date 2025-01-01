50b1d74b8f5d550679438299d33359c11f741202
ALDRIN REIS DE MORAIS <40066614+aldrinreis@users.noreply.github.com>
2024-12-31 23:04:58 -0300

    java

---
+++ src/twoExerciciosEstruturaCondicional/dProblemaOperadora.java
@@ -0,0 +1,34 @@
+package twoExerciciosEstruturaCondicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+Uma operadora de telefonia cobra R$ 50.00 por um plano básico que dá direito a 100 minutos de telefone.
+Cada minuto que exceder a franquia de 100 minutos custa R$ 2.00.
+Fazer um programa para ler a quantidade de minutos que uma pessoa consumiu, daí mostrar o valor a ser pago.
+ */
+
+public class dProblemaOperadora {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int minutos;
+        double valor;
+
+        System.out.print("Digite a quantidade de minutos: ");
+        minutos = sc.nextInt();
+        valor = 50.0;
+
+        if(minutos > 100){
+            valor = valor + (minutos - 100) * 2;
+
+        }
+
+        System.out.printf("Valor a pagar: R$ %.2f%n", valor);
+
+
+        sc.close();
+    }
+}
---
+++ src/twoExerciciosEstruturaCondicional/eProblemaTrocoVerificado.java
@@ -0,0 +1,46 @@
+package twoExerciciosEstruturaCondicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+Fazer um programa para calcular o troco no processo de pagamento de um produto de uma mercearia.
+O programa deve ler o preço unitário do produto, a quantidade de unidades compradas deste produto,
+e o valor em dinheiro dado pelo cliente. Seu programa deve mostrar o valor do troco a ser devolvido
+ao cliente. Se o dinheiro dado pelo cliente não for suficiente, mostrar uma mensagem informando o
+valor restante conforme exemplo.
+ */
+
+public class eProblemaTrocoVerificado {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double preco, dinheiro, troco, faltam;
+        int qtde;
+
+        System.out.print("Preço unitário do produto: ");
+        preco = sc.nextDouble();
+
+        System.out.print("Quantidade comprada: ");
+        qtde = sc.nextInt();
+
+        System.out.print("Dinheiro recebido: ");
+        dinheiro = sc.nextDouble();
+
+
+        if(qtde * preco < dinheiro){
+            troco = dinheiro - (preco * qtde);
+            System.out.printf("TROCO = %.2f%n", troco);
+        }else{
+            faltam = (preco * qtde) - dinheiro;
+
+            System.out.printf("DINHEIRO INSUFICIENTE. FALTAM %.2f REAIS %n", faltam);
+        }
+
+
+
+
+        sc.close();
+    }
+}
